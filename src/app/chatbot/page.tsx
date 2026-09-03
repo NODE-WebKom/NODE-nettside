3dbbe75310c98849a9e43b8882d4792ea4ef4cf3
Magnus <magnusapaulsen@gmail.com>
2026-09-03 17:24:20 +0200

    Removed the old Chatbot and created a new one with Vector Storage in OpenAI API and the new File Search tool.

--- src/app/chatbot/page.tsx
+++
@@ -1,9 +0,0 @@
-import Chatbot from "@/components/Chatbot";
-
-export default function Home() {
-    return(
-        <div className=" flex items-center h-screen justify-center bg-gray-300">
-            <Chatbot />
-        </div>
-    )
-}
